cacd7de6623cdba4743fc851a40b6f93cb26b195
Yarden Katz <yarden.katz@gmail.com>
2015-02-26 14:12:44 -0500

    Rain HMM model

--- particlefever/models/rain_hmm_model.py
+++ particlefever/models/rain_hmm_model.py
@@ -2,28 +2,40 @@
 ## Rain-Umbrella 
 ##
 import pymc
-from pymc import DiscreteUniform, Exponential, deterministic, Poisson, Uniform
 import numpy as np
 
-def make_model():
-    @pymc.stochastic(dtype=bool)
-    def rain(value=False):
-        """
-        Rain node. Initial value is False.
-        """
-        # initial rain probability is random
-        return pymc.bernoulli_like(value, 0.5)
+rain = pymc.Bernoulli("rain", 0.5)
 
-    @pymc.stochastic(dtype=bool)
-    def umbrella(value=False):
-        """
-        Umbrella node. Initial value is False.
-        """
-        # initial umbrella probability is random
-        return pymc.bernoulli_like(value, 0.5)
-    
-    return locals()
+def umbrella_logp(value, rain):
+    """
+    Umbrella node. Initial value is False.
+
+    P(umbrella=True | rain=True)  = 0.9 
+    P(umbrella=True | rain=False) = 0.2
+    """
+    p_umb_given_rain = 0.9
+    p_umb_given_no_rain = 0.2
+    if rain:
+        logp = pymc.bernoulli_like(value, p_umb_given_rain)
+    else:
+        logp = pymc.bernoulli_like(value, p_umb_given_no_rain)
+    return logp
 
 
-rain_hmm = pymc.Model(make_model())
-print "rain hmm: ", rain_hmm
+umbrella = pymc.Stochastic(name="umbrella",
+                           doc="umbrella var",
+                           parents={"rain": rain},
+                           logp=umbrella_logp,
+                           observed=True,
+                           value=True)
+rain_hmm = pymc.Model([rain, umbrella])
+
+if __name__ == "__main__":
+    # Run inference
+    m = pymc.MCMC(rain_hmm)
+    m.sample(iter=5000)
+    print "\n"
+    print "rain: "
+    print np.mean(m.trace("rain")[:])
+
+
