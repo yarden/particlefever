51bc374df59fca916ef0192c4f2829450f5a1b28
Yarden Katz <yarden.katz@gmail.com>
2016-02-05 01:08:22 -0500

    revs

---
+++ particlefever/state.pyx
@@ -0,0 +1,21 @@
+##
+## State representation
+##
+import os
+import sys
+import time
+
+import particlefever
+
+
+class State:
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
+
+        
+
