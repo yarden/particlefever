0505c27de72698cf4438cfc95b5cfdb2254ccdc1
Yarden Katz <yarden.katz@gmail.com>
2016-02-07 18:23:23 -0500

    revs

---
+++ particlefever/hdp.py
@@ -0,0 +1,19 @@
+##
+## Hierarchical Dirichlet process
+##
+import os
+import sys
+import time
+
+class HierarchicalDP:
+    """
+    Hierarchical Dirichlet process.
+    """
+    def __init__(self):
+        pass
+
+    def foo():
+        pass
+
+
+
